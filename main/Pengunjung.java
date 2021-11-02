741fd2df8667502f5ddcbfc3b4badcb6587f573a
RaflyDioniswaraPramono <92341622+RaflyDioniswaraPramono@users.noreply.github.com>
2021-11-02 09:50:41 +0700

    Add files via upload

---
+++ main/Pengunjung.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Pengunjung {
+    public String namaLengkap;
+    public String asalKota;
+    public String nomorPonsel;
+    public String cekNomorPonsel;
+
+public Pengunjung(String namaLengkap, String asalKota, String nomorPonsel) {
+    this.namaLengkap = namaLengkap;
+    this.asalKota = asalKota;
+    this.nomorPonsel = nomorPonsel;    
+    
+    }
+}
+
